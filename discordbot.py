98bd52c77121be05a79392649b3f88231403a300
UMRchan <58333082+UMRchan@users.noreply.github.com>
2019-11-29 22:18:02 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -6,6 +6,7 @@ bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
+
 @bot.event
 async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
@@ -16,6 +17,9 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
+    
+
+    
 
 
 bot.run(token)
