e965023b02aefddab40b43385bfbf079636c9edd
UMRchan <58333082+UMRchan@users.noreply.github.com>
2019-11-29 22:27:54 +0900

    discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -16,6 +16,9 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
-
+    
+@bot.command()
+async def  ピザ(ctx):
+    await ctx.send('サイズはLL')
 
 bot.run(token)
